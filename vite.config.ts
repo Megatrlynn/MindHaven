748083d5fe8e48e97c967115cd3a2cd3c0773fab
Megatrlynn <lynnslyna@gmail.com>
2026-04-11 06:29:36 +0200

    Remove vite-plugin-sitemap - use static files instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import Pages from 'vite-plugin-pages'
-import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
     Pages(),
-    Sitemap({
-      hostname: 'https://mind-haven.netlify.app',
-    })
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
